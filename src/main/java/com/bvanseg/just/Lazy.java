e7a29f025f694aaa2e9dfc58407cf71b30601e27
bvanseg <bvanseg@gmail.com>
2025-03-22 19:00:29 -0400

    ✨ Added Lazy.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ src/main/java/com/bvanseg/just/Lazy.java
@@ -0,0 +1,35 @@
+package com.bvanseg.just;
+
+import java.util.function.Supplier;
+
+public class Lazy<T> implements Supplier<T> {
+
+    public static <T> Lazy<T> of(Supplier<T> supplier) {
+        return new Lazy<>(supplier);
+    }
+
+    private final Supplier<T> supplier;
+
+    private T value;
+
+    private Lazy(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    public T get() {
+        if (value == null) {
+            value = supplier.get();
+        }
+
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return !isPresent();
+    }
+
+    public boolean isPresent() {
+        return value != null;
+    }
+}
