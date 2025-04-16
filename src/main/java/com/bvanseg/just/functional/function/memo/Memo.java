502a1d1583b98b45064b4d13c2a794474380fbbb
bvanseg <bvanseg@gmail.com>
2025-04-15 23:16:21 -0400

    ✨ Added Memo.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ src/main/java/com/bvanseg/just/functional/function/memo/Memo.java
@@ -0,0 +1,36 @@
+package com.bvanseg.just.functional.function.memo;
+
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+public class Memo<A, R> implements Function<A, R> {
+
+    private A aRef;
+
+    private R cachedResult;
+
+    private final Function<A, R> function;
+
+    private final BiPredicate<A, A> aEq;
+
+    public Memo(Function<A, R> function) {
+        this(function, (oldRef, newRef) -> newRef == oldRef);
+    }
+
+    public Memo(Function<A, R> function, BiPredicate<A, A> aEq) {
+        this.function = function;
+        this.aEq = aEq;
+    }
+
+    @Override
+    public R apply(A a) {
+        if (aEq.test(aRef, a)) {
+            return cachedResult;
+        }
+
+        cachedResult = function.apply(a);
+        aRef = a;
+
+        return cachedResult;
+    }
+}
