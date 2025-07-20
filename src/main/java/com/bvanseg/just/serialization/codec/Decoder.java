cdf8d78520eada84740c1cff8da2199eb6788eb6
bvanseg <bvanseg@gmail.com>
2025-07-20 11:09:08 -0400

    ✨ Added Decoder#map.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/bvanseg/just/serialization/codec/Decoder.java
+++ src/main/java/com/bvanseg/just/serialization/codec/Decoder.java
@@ -3,7 +3,23 @@ package com.bvanseg.just.serialization.codec;
 import com.bvanseg.just.functional.result.Result;
 import com.bvanseg.just.serialization.codec.schema.CodecSchema;
 
+import java.util.function.Function;
+
 public interface Decoder<A> {
 
     <T> Result<A, T> decode(CodecSchema<T> codecSchema, T input);
+
+    default <B> Decoder<B> map(Function<? super A, ? extends B> function) {
+        return new Decoder<>() {
+            @Override
+            public <T> Result<B, T> decode(CodecSchema<T> codecSchema, T input) {
+                return Decoder.this.decode(codecSchema, input).map(function);
+            }
+
+            @Override
+            public String toString() {
+                return Decoder.this + "[mapped]";
+            }
+        };
+    }
 }
