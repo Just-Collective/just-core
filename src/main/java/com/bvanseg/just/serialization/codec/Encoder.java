8b656607df7b19d111cfe795c16eda73d55f8782
bvanseg <bvanseg@gmail.com>
2025-07-20 11:08:46 -0400

    ✨ Added Encoder#contravariantMap.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/bvanseg/just/serialization/codec/Encoder.java
+++ src/main/java/com/bvanseg/just/serialization/codec/Encoder.java
@@ -2,7 +2,23 @@ package com.bvanseg.just.serialization.codec;
 
 import com.bvanseg.just.serialization.codec.schema.CodecSchema;
 
+import java.util.function.Function;
+
 public interface Encoder<A> {
 
     <T> T encode(CodecSchema<T> codecSchema, A value);
+
+    default <B> Encoder<B> contravariantMap(Function<? super B, ? extends A> function) {
+        return new Encoder<>() {
+            @Override
+            public <T> T encode(CodecSchema<T> codecSchema, B input) {
+                return Encoder.this.encode(codecSchema, function.apply(input));
+            }
+
+            @Override
+            public String toString() {
+                return Encoder.this + "[contravariantMapped]";
+            }
+        };
+    }
 }
